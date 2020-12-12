0649fa31316f4b95c2b1712f4de4bde4e0e6f9fd
Stephano <stephanobryan456@hotmail.com>
2020-12-11 22:15:58 -0500

    correcciones

--- frontend/src/app/models/presupuesto.model.ts
+++ frontend/src/app/models/presupuesto.model.ts
@@ -4,11 +4,11 @@ export class Presupuesto{
 
   }
   public _id: string;
-  public asunto: number;
+  public asunto: string;
   public mes: string;
   public fechaPago: Date;
   public monto: number;
-  public estado: number;
-  public tipoAsunto: number;
+  public estado: string;
+  public tipoAsunto: string;
 
 }
