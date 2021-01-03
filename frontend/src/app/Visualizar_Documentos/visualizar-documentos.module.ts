bdd0daea7ef389d9704c95cf82e7771b29fcd642
Renzo Reccio <reccio_2001@hotmail.com>
2021-01-03 18:39:22 -0500

    visualizar documentos

--- frontend/src/app/Visualizar_Documentos/visualizar-documentos.module.ts
+++ frontend/src/app/Visualizar_Documentos/visualizar-documentos.module.ts
@@ -4,11 +4,14 @@ import { CommonModule } from "@angular/common";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { ChartsModule } from "ng2-charts";
 import { VisualizarDocumentosRoutingModule } from './visualizar-documentos-routing.module';
+import { VisualizarDocComponent } from './visualizar-doc/visualizar-doc.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { Ng2SearchPipeModule } from "ng2-search-filter";
 
 @NgModule({
-  imports: [CommonModule, VisualizarDocumentosRoutingModule, NgbModule, ChartsModule],
+  imports: [CommonModule, VisualizarDocumentosRoutingModule,NgbModule, ChartsModule, FormsModule, ReactiveFormsModule, Ng2SearchPipeModule],
   exports: [],
-  declarations: [],
+  declarations: [VisualizarDocComponent],
   providers: [],
 })
 export class VisualizarDocumentosModule {}
